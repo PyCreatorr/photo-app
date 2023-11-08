2acc5e1b65da9ff63e72f98aaec32c81d093221b
PyCreatorr <ttptv@yahoo.com>
2023-11-08 23:40:59 +0100

    draft, published, scheduled blog_posts

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -2,7 +2,8 @@ class BlogPostsController < ApplicationController
     before_action :authenticate_user!, except: [:index, :show]
     before_action :set_blog_post, except: [:index, :new, :create,]
     def index
-        @blog_posts = BlogPost.all
+        #@blog_posts = BlogPost.all
+        @blog_posts = user_signed_in? ? BlogPost.sorted : BlogPost.published.sorted
     end
 
     def show
@@ -78,14 +79,15 @@ class BlogPostsController < ApplicationController
     private
 
     def set_blog_post
-        @blog_post = BlogPost.find(params[:id])
+        #@blog_post = BlogPost.published.find(params[:id])
+        @blog_post = user_signed_in? ? BlogPost.find(params[:id]) : BlogPost.published.find(params[:id])
         rescue ActiveRecord::RecordNotFound
             redirect_to blog_posts_path
             flash[:danger] = "This Blog Post with id = #{params[:id]} doesnt exsists!"
     end
 
     def blog_post_params 
-        params.require(:blog_post).permit(:title, :body)
+        params.require(:blog_post).permit(:title, :body, :published_at)
     end
 
 end
